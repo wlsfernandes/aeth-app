21bd8cf93258278b1a737bfc0fa7f027ed56b8fa
wlsfernandes@gmail.com <wlsfernandes@gmail.com>
2024-11-20 16:53:06 -0500

    fix payment screen

--- resources/views/pages/payment-membership.blade.php
+++ resources/views/pages/payment-membership.blade.php
@@ -36,7 +36,7 @@
                 </ul>
             </div>
         @endif
-        <div class="card">
+        <div class="card" style="margin-bottom: 100px;">
             <span><b>@lang('header.choose_payment')</b></span>
             <div class="accordion" id="accordionExample" style="color:#4A235A;margin-top:20px;">
                 <div class="card">
