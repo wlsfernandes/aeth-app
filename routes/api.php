25855072a72c664411f5eb4686f6c104b4b1f955
wlsfernandes@gmail.com <wlsfernandes@gmail.com>
2024-12-13 17:47:07 -0500

    remove upsservice

--- routes/api.php
+++ routes/api.php
@@ -14,26 +14,6 @@ use App\Services\UPSService;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-
-Route::post('/ups/rate', function (Request $request) {
-    try {
-        $shipmentDetails = $request->all();
-
-        $upsService = new UPSService();
-        $rates = $upsService->getRates($shipmentDetails);
-
-        return response()->json([
-            'status' => 'success',
-            'data' => $rates
-        ]);
-    } catch (\Exception $e) {
-        return response()->json([
-            'status' => 'error',
-            'message' => $e->getMessage()
-        ], 500);
-    }
-});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
