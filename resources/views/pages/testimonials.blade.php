75a667e4bc6fb2bd559ba13c5cd3c55683e4499a
wlsfernandes@gmail.com <wlsfernandes@gmail.com>
2024-12-17 15:09:23 -0500

    testmonials  fix

--- resources/views/pages/testimonials.blade.php
+++ resources/views/pages/testimonials.blade.php
@@ -9,62 +9,7 @@
 
 <!-- Content here -->
 @section('content') 
-<section class="page-title centred">
 
-    <div class="container px-4 py-5" id="hanging-icons">
-        <h2 class="pb-2 border-bottom">Hanging icons</h2>
-        <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
-            <div class="col d-flex align-items-start">
-                <div class="icon-square bg-light text-dark flex-shrink-0 me-3">
-                    <svg class="bi" width="1em" height="1em">
-                        <use xlink:href="#toggles2"></use>
-                    </svg>
-                </div>
-                <div>
-                    <h2>Featured title</h2>
-                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
-                        sentence
-                        and probably just keep going until we run out of words.</p>
-                    <a href="#" class="btn btn-primary">
-                        Primary button
-                    </a>
-                </div>
-            </div>
-            <div class="col d-flex align-items-start">
-                <div class="icon-square bg-light text-dark flex-shrink-0 me-3">
-                    <svg class="bi" width="1em" height="1em">
-                        <use xlink:href="#cpu-fill"></use>
-                    </svg>
-                </div>
-                <div>
-                    <h2>Featured title</h2>
-                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
-                        sentence
-                        and probably just keep going until we run out of words.</p>
-                    <a href="#" class="btn btn-primary">
-                        Primary button
-                    </a>
-                </div>
-            </div>
-            <div class="col d-flex align-items-start">
-                <div class="icon-square bg-light text-dark flex-shrink-0 me-3">
-                    <svg class="bi" width="1em" height="1em">
-                        <use xlink:href="#tools"></use>
-                    </svg>
-                </div>
-                <div>
-                    <h2>Featured title</h2>
-                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
-                        sentence
-                        and probably just keep going until we run out of words.</p>
-                    <a href="#" class="btn btn-primary">
-                        Primary button
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
 
 <section class="two-column-section" style="background-color: #ffffff; padding: 40px 0;">
     <div class="auto-container">
