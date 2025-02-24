d7b87d5756b61c62250502b3170a7036a5dc0cad
wlsfernandes <wlsfernandes@gmail.com>
2025-02-24 15:03:25 -0500

    change membership certification

--- app/Http/Controllers/CertificationController.php
+++ app/Http/Controllers/CertificationController.php
@@ -8,45 +8,88 @@ use Illuminate\Http\Request;
 
 class CertificationController extends Controller
 {
+
     public function generateImage()
     {
         $user = Auth::user();
         $member = Member::where('user_id', $user->id)->first();
-        // Create a base image
+
+        // Load the certificate template
         $imagePath = public_path('certificates/AETH_Membership.png');
-        // Load the image
         $img = Image::make($imagePath);
 
-        $fontSize = 70;
-        $fontSize2 = 40;
-        $fontColor = '#000000';
-        $name = !empty($member->last_name) && !empty($member->first_name) ? $member->first_name . ' ' . $member->last_name : (!empty($user->name) ? $user->name : '');
-        $membershipInfo = !empty($member) ? 'Member ID: ' . $member->id . '   Expiration Date: ' . $member->membership_end_date : 'Member ID 98765  Expiration Date: 18/11/2025'; 
-        // Membership No. 13139 Valid from 11/14/2024 to 11/14/2025
-        $img->text($name ?? '', $img->width() / 2, $img->height() / 2, function ($font) use ($fontSize, $fontColor) {
-            $font->file(public_path('assets/fonts/arial.ttf'));
-            $font->size($fontSize);
+        // Font settings
+        $fontSizeName = 96;
+        $fontSizeDetails = 48;
+        $fontColor = '#4a235a';
+        $fontBasicPath = public_path('assets/fonts/arial.ttf');
+        $fontPath = public_path('assets/fonts/AlexBrush-Regular.ttf');
+        if (!file_exists($fontPath)) {
+            $fontPath = public_path('assets/fonts/arial.ttf');
+        }
+
+
+        // Member's full name
+        $name = !empty($member->last_name) && !empty($member->first_name) ?
+            $member->first_name . ' ' . $member->last_name : (!empty($user->name) ? $user->name : '');
+
+        // Membership ID
+        $membershipID = !empty($member) ? $member->id : '';
+        // Membership Date
+        if (!empty($member)) {
+            $startDate = optional($member->membership_start_date)->format('M/d/Y') ?? '';
+            $endDate = optional($member->membership_end_date)->format('M/d/Y') ?? '';
+            $membershipInfo = trim("{$startDate}   -   {$endDate}");
+        } else {
+            $membershipInfo = '';
+        }
+
+        $membership_plan = !empty($member) ? $member->membership_plan : '';
+
+        // Member Name Position
+        $nameX = $img->width() / 2;
+        $nameY = $img->height() * 0.3;
+        $img->text($name ?? '', $nameX, $nameY, function ($font) use ($fontSizeName, $fontColor, $fontPath) {
+            $font->file($fontPath);
+            $font->size($fontSizeName);
+            $font->color($fontColor);
+            $font->align('center');
+            $font->valign('center');
+        });
+        // Member ID Position
+        $membershipIDY = $nameY + ($fontSizeName * 3.0);
+        $img->text($membershipID, $nameX, $membershipIDY, function ($font) use ($fontSizeDetails, $fontColor, $fontPath) {
+            $font->file($fontPath);
+            $font->size($fontSizeDetails);
+            $font->color($fontColor);
+            $font->align('center');
+            $font->valign('center');
+        });
+        // Member Plan Position
+        $membershipPlanx = $nameX + ($fontSizeName * 0);
+        $membershipPlanY = $membershipIDY + ($fontSizeName * 1.2);
+        $img->text($membership_plan, $membershipPlanx, $membershipPlanY, function ($font) use ($fontSizeDetails, $fontColor, $fontPath) {
+            $font->file($fontPath);
+            $font->size($fontSizeDetails);
             $font->color($fontColor);
             $font->align('center');
             $font->valign('center');
         });
 
-        // Calculate the position for the second line of text
-        $lineHeight = $fontSize * 1.5;  // Adjust line height based on font size
-        $yPositionForSecondLine = $img->height() / 2 + $lineHeight;  // Position it below the first line
 
-        // Draw the second line of text (membership info)
-        $img->text($membershipInfo, $img->width() / 2, $yPositionForSecondLine, function ($font) use ($fontSize2, $fontColor) {
-            $font->file(public_path('assets/fonts/arial.ttf'));
-            $font->size($fontSize2);
+        // Member Info Start Date End Date
+        $membershipx = $nameX + ($fontSizeName * 1.4);
+        $membershipY = $membershipIDY + ($fontSizeName * 2.7);
+        $img->text($membershipInfo, $membershipx, $membershipY, function ($font) use ($fontSizeDetails, $fontColor, $fontPath) {
+            $font->file($fontPath);
+            $font->size($fontSizeDetails);
             $font->color($fontColor);
             $font->align('center');
             $font->valign('center');
         });
-
-
 
         return $img->response('png')->header('Content-Disposition', 'attachment; filename="membership_image.png"');
-   
     }
+
+
 }
