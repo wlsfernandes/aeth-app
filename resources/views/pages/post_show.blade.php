a07d0f684714ea8061b084df81064aa9ac6c9891
wlsfernandes <wlsfernandes@gmail.com>
2025-03-31 15:20:46 -0400

    change blog

--- resources/views/pages/post_show.blade.php
+++ resources/views/pages/post_show.blade.php
@@ -19,73 +19,90 @@
     <div class="row">
         <div class="col-md-12">
             <div class="card mb-12 box-shadow">
-                <img class="card-img-top"
+
+                {{-- Title & Date --}}
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h1 style="color:#4a235a">
+                            @switch(App::getLocale())
+                                @case('es')
+                                    {{ $post->title_es ?? '' }}
+                                    @break
+                                @case('pt')
+                                    {{ $post->title_pt ?? '' }}
+                                    @break
+                                @default
+                                    {{ $post->title_en ?? '' }}
+                            @endswitch
+                        </h1>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="d-flex justify-content-end pe-3">
+                            <small class="text-muted">{{ $post->date }}</small>
+                        </div>
+                    </div>
+                </div>
+
+                {{-- Image --}}
+                <img class="card-img-top mt-4"
                      src="{{ $post->image_url }}"
                      alt="{{ $post->title_en }}">
-                <div class="card-body" style="min-height: 300px; display: flex; flex-direction: column; justify-content: space-between;">
-                    <h2>
-                        @switch(App::getLocale())
-                            @case('es')
-                                {{ $post->title_es ?? '' }}
-                                @break
-                            @case('pt')
-                                {{ $post->title_pt ?? '' }}
-                                @break
-                            @default
-                                {{ $post->title_en ?? '' }}
-                        @endswitch
-                    </h2>
 
-                    <p class="card-text" style="margin-top: 50px;">
+                {{-- Content --}}
+                <div class="card-body" style="min-height: 300px; display: flex; flex-direction: column; justify-content: space-between;">
+                    <p class="card-text mt-4">
                         @switch(App::getLocale())
                             @case('es')
                                 {{ $post->content_es ?? '' }}
                                 @break
                             @case('pt')
-                                {{ $post->content_pt ?? ''}}
+                                {{ $post->content_pt ?? '' }}
                                 @break
                             @default
                                 {{ $post->content_en ?? '' }}
                         @endswitch
                     </p>
-                    <p class="card-text" style="margin-top: 50px;">
+
+                    <p class="card-text mt-4">
                         @switch(App::getLocale())
                             @case('es')
-                                {{ $post->summary_es ?? ''}}
+                                {{ $post->summary_es ?? '' }}
                                 @break
                             @case('pt')
-                                {{ $post->summary_pt ?? ''}}
+                                {{ $post->summary_pt ?? '' }}
                                 @break
                             @default
-                                {{ $post->summary_en ?? ''}}
+                                {{ $post->summary_en ?? '' }}
                         @endswitch
                     </p>
-                    <small class="text-muted">{{ $post->date }}</small>
 
-                    <div class="d-flex justify-content-center align-items-center" style="margin-top: 75px;">
-                        @if($post->external_link)
+                    {{-- External Link Button --}}
+                    @if($post->external_link)
+                        <div class="d-flex justify-content-center mt-5">
                             <a href="{{ $post->external_link }}" target="_blank" class="btn btn-danger btn-md">
                                 {{ $post->external_link_button ?? 'Click here to access' }}
                             </a>
-                        @endif
-                    </div>
-                    
+                        </div>
+                    @endif
+
+                    {{-- Download Buttons --}}
                     @if($post->file_url)
-                        <div class="d-flex justify-content-center align-items-center" style="margin-top: 50px;">
+                        <div class="d-flex justify-content-center mt-4">
                             <a href="{{ $post->file_url }}" target="_blank" class="btn btn-primary btn-md">
-                                {{ $post->button_text_en ? $post->button_text_en : 'Download English here' }}
+                                {{ $post->button_text_en ?? 'Download English here' }}
                             </a>
                         </div>
                     @endif
-                    
+
                     @if($post->file_url_es)
-                        <div class="d-flex justify-content-center align-items-center" style="margin-top: 50px;">
+                        <div class="d-flex justify-content-center mt-4">
                             <a href="{{ $post->file_url_es }}" target="_blank" class="btn btn-primary btn-md">
-                                {{ $post->button_text_es ? $post->button_text_es : 'Descargar Español aquí' }}
+                                {{ $post->button_text_es ?? 'Descargar Español aquí' }}
                             </a>
                         </div>
                     @endif
-                   
+
                 </div> <!-- end card-body -->
             </div> <!-- end card -->
         </div>
