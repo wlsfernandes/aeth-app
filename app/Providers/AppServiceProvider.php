cb14393e12c4acd0e32d1bf98292cfc2a02a3d2c
wlsfernandes <wlsfernandes@gmail.com>
2025-04-22 09:19:02 -0400

    redirecting site

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Redirect;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,8 +21,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Force HTTPS in production
         if (app()->environment('production')) {
             URL::forceScheme('https');
+        }
+
+        // Redirect all other domains to somosaeth.org
+        if (!app()->runningInConsole()) {
+            $host = request()->getHost();
+
+            if (in_array($host, ['www.aeth.org', 'aeth.org'])) {
+                Redirect::to('https://somosaeth.org' . request()->getRequestUri(), 301)->send();
+            }
         }
     }
 }
