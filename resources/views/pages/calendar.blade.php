0988247543acf42fd1a5913e0ceb4fa81abe6826
wlsfernandes <wlsfernandes@gmail.com>
2025-06-04 15:19:23 -0400

    events calendar update

--- resources/views/pages/calendar.blade.php
+++ resources/views/pages/calendar.blade.php
@@ -12,18 +12,16 @@
 @section('content')
 
 
-    <div class="slider-area rbt-banner-10 height-750 bg_image bg_image--11" data-black-overlay="5">
+    <div class="slider-area rbt-banner-10 height-750 bg_image" data-black-overlay="5"
+        style="background-image: url('{{ asset('assets/images/events/event.jpg') }}')">
+
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
                     <div class="inner text-center">
                         <div class="section-title mb--20">
-                            <span class="subtitle bg-coral-opacity">How We Work</span>
+                            <span class="subtitle bg-coral-opacity">@lang('messages.calendar')</span>
                         </div>
-                        <h1 class="title display-one text-white">@lang('messages.welcome') *** Take Challenge for Build Your
-                            Life. <br>The World Most
-                            Lessons for Back to Your Life.</h1>
-
                     </div>
                 </div>
             </div>
@@ -35,193 +33,73 @@
         <div class="container">
             <div class="row g-5">
                 <!-- Start Single Event  -->
-                <div class="col-lg-6 col-md-6 col-12">
-                    <div class="rbt-card card-list-2 event-list-card variation-01 rbt-hover">
-                        <div class="rbt-card-img">
-                            <a href="">
-                                <img src="{{ asset('assets/template/images/event/grid-type-01.jpg') }}" alt="Card image">
-                            </a>
-                        </div>
-                        <div class="rbt-card-body">
-                            <ul class="rbt-meta">
-                                <li><i class="feather-calendar"></i>11 Jan, 2024</li>
-                                <li><i class="feather-map-pin"></i>IAC Building</li>
-                            </ul>
-                            <h4 class="rbt-card-title"><a href="">International Education Fair
-                                    2024</a></h4>
-                            <div class="read-more-btn">
-                                <a class="rbt-btn btn-border hover-icon-reverse btn-sm radius-round" href="">
-                                    <span class="icon-reverse-wrapper">
-
-                                    </span>
+                @foreach($posts as $post)
+                    <div class="col-lg-6 col-md-6 col-12">
+                        <div class="rbt-card card-list-2 event-list-card variation-01 rbt-hover">
+                            <div class="rbt-card-img">
+                                <a href="{{ route('post.show', $post->slug) }}">
+                                    <img src="{{ $post->image_url }}" alt="{{ $post->title_en }}"
+                                        style="object-fit: cover; height: 250px; width: 100%;">
                                 </a>
+                            </div>
+                            <div class="rbt-card-body">
+                                <ul class="rbt-meta">
+                                    <li><i class="feather-calendar"></i>{{ $post->date }}</li>
+                                    <li><i class="feather-map-pin"></i>{{ $post->location ?? 'Online' }}</li>
+                                </ul>
+                                <h4 class="rbt-card-title">
+                                    <a href="{{ route('post.show', $post->slug) }}">
+                                        @if(App::getLocale() == 'es')
+                                            {{ $post->title_es }}
+                                        @elseif(App::getLocale() == 'pt-BR')
+                                            {{ $post->title_pt }}
+                                        @else
+                                            {{ $post->title_en }}
+                                        @endif
+                                    </a>
+                                </h4>
+                                <p class="text-muted small">{{ $post->plain_summary }}</p>
+                                <div class="read-more-btn mt-2">
+                                    <a class="rbt-btn btn-border hover-icon-reverse btn-sm radius-round"
+                                        href="{{ route('post.show', $post->slug) }}">
+                                        <span class="icon-reverse-wrapper">
+                                            <span class="btn-text">@lang('blog.read_more')</span>
+                                            <span class="btn-icon"><i class="feather-arrow-right"></i></span>
+                                            <span class="btn-icon"><i class="feather-arrow-right"></i></span>
+                                        </span>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <!-- End Single Event  -->
-                <!-- Start Single Event  -->
-                <div class="col-lg-6 col-md-6 col-12">
-                    <div class="rbt-card card-list-2 event-list-card variation-01 rbt-hover">
-                        <div class="rbt-card-img">
-                            <a href="">
-                                <img src="{{ asset('assets/template/images/event/grid-type-02.jpg') }}" alt="Card image">
-                            </a>
-                        </div>
-                        <div class="rbt-card-body">
-                            <ul class="rbt-meta">
-                                <li><i class="feather-map-pin"></i>Vancouver</li>
-                                <li><i class="feather-clock"></i>8:00 am - 5:00 pm</li>
-                            </ul>
-                            <h4 class="rbt-card-title"><a href="">Painting Art Contest 2020</a>
-                            </h4>
-
-                            <div class="read-more-btn">
-                                <a class="rbt-btn btn-border hover-icon-reverse btn-sm radius-round" href="">
-
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
                 <!-- End Single Event  -->
 
-                <!-- Start Single Event  -->
-                <div class="col-lg-6 col-md-6 col-12">
-                    <div class="rbt-card card-list-2 event-list-card variation-01 rbt-hover">
-                        <div class="rbt-card-img">
-                            <a href="">
-                                <img src="{{ asset('assets/template/images/event/grid-type-03.jpg') }}" alt="Card image">
-                            </a>
-                        </div>
-                        <div class="rbt-card-body">
-                            <ul class="rbt-meta">
-                                <li><i class="feather-map-pin"></i>Paris</li>
-                                <li><i class="feather-clock"></i>8:00 am - 5:00 pm</li>
-                            </ul>
-                            <h4 class="rbt-card-title"><a href="">Histudy Education Fair
-                                    2024</a>
-                            </h4>
+                <div class="row">
+                    <div class="pagination-wrapper centred">
+                        <ul class="pagination clearfix">
+                            @if ($posts->onFirstPage())
+                                <li><a href="#" aria-disabled="true"><i class="icon-56"></i></a></li>
+                            @else
+                                <li><a href="{{ $posts->previousPageUrl() }}"><i class="icon-56"></i></a></li>
+                            @endif
 
-                            <div class="read-more-btn">
-                                <a class="rbt-btn btn-border hover-icon-reverse btn-sm radius-round" href="">
-                                    <span class="icon-reverse-wrapper">
-                                        <span class="btn-text">Get Ticket</span>
-                                        <span class="btn-icon"><i class="feather-arrow-right"></i></span>
-                                        <span class="btn-icon"><i class="feather-arrow-right"></i></span>
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- End Single Event  -->
+                            @foreach ($posts->getUrlRange(1, $posts->lastPage()) as $page => $url)
+                                <li>
+                                    <a href="{{ $url }}" class="{{ $posts->currentPage() === $page ? 'current' : '' }}">
+                                        {{ $page }}
+                                    </a>
+                                </li>
+                            @endforeach
 
-                <!-- Start Single Event  -->
-                <div class="col-lg-6 col-md-6 col-12">
-                    <div class="rbt-card card-list-2 event-list-card variation-01 rbt-hover">
-                        <div class="rbt-card-img">
-                            <a href="">
-                                <img src="{{ asset('assets/template/images/event/grid-type-04.jpg') }}" alt="Card image">
-                            </a>
-                        </div>
-                        <div class="rbt-card-body">
-                            <ul class="rbt-meta">
-                                <li><i class="feather-map-pin"></i>IAC Building</li>
-                                <li><i class="feather-clock"></i>8:00 am - 5:00 pm</li>
-                            </ul>
-                            <h4 class="rbt-card-title"><a href=""> Elegant Light Box Paper Cut
-                                    Dioramas</a></h4>
-
-                            <div class="read-more-btn">
-                                <a class="rbt-btn btn-border hover-icon-reverse btn-sm radius-round" href="">
-                                    <span class="icon-reverse-wrapper">
-                                        <span class="btn-text">Get Ticket</span>
-                                        <span class="btn-icon"><i class="feather-arrow-right"></i></span>
-                                        <span class="btn-icon"><i class="feather-arrow-right"></i></span>
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- End Single Event  -->
-
-                <!-- Start Single Event  -->
-                <div class="col-lg-6 col-md-6 col-12">
-                    <div class="rbt-card card-list-2 event-list-card variation-01 rbt-hover">
-                        <div class="rbt-card-img">
-                            <a href="">
-                                <img src="{{ asset('assets/template/images/event/grid-type-05.jpg') }}" alt="Card image">
-                            </a>
-                        </div>
-                        <div class="rbt-card-body">
-                            <ul class="rbt-meta">
-                                <li><i class="feather-map-pin"></i>Vancouver</li>
-                                <li><i class="feather-clock"></i>8:00 am - 5:00 pm</li>
-                            </ul>
-                            <h4 class="rbt-card-title"><a href="">Most Effective Ways
-                                    Education's
-                                    Problem.</a></h4>
-
-                            <div class="read-more-btn">
-                                <a class="rbt-btn btn-border hover-icon-reverse btn-sm radius-round" href="">
-                                    <span class="icon-reverse-wrapper">
-                                        <span class="btn-text">Get Ticket</span>
-                                        <span class="btn-icon"><i class="feather-arrow-right"></i></span>
-                                        <span class="btn-icon"><i class="feather-arrow-right"></i></span>
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- End Single Event  -->
-
-                <!-- Start Single Event  -->
-                <div class="col-lg-6 col-md-6 col-12">
-                    <div class="rbt-card card-list-2 event-list-card variation-01 rbt-hover">
-                        <div class="rbt-card-img">
-                            <a href="">
-                                <img src="{{ asset('assets/template/images/event/grid-type-06.jpg') }}" alt="Card image">
-                            </a>
-                        </div>
-                        <div class="rbt-card-body">
-                            <ul class="rbt-meta">
-                                <li><i class="feather-map-pin"></i>Paris</li>
-                                <li><i class="feather-clock"></i>8:00 am - 5:00 pm</li>
-                            </ul>
-                            <h4 class="rbt-card-title"><a href="">Top 7 Common About
-                                    Education.</a>
-                            </h4>
-
-                            <div class="read-more-btn">
-                                <a class="rbt-btn btn-border hover-icon-reverse btn-sm radius-round" href="">
-                                    <span class="icon-reverse-wrapper">
-                                        <span class="btn-text">Get Ticket</span>
-                                        <span class="btn-icon"><i class="feather-arrow-right"></i></span>
-                                        <span class="btn-icon"><i class="feather-arrow-right"></i></span>
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- End Single Event  -->
-            </div>
-            <div class="row">
-                <div class="col-lg-12 mt--60">
-                    <nav>
-                        <ul class="rbt-pagination">
-                            <li><a href="#" aria-label="Previous"><i class="feather-chevron-left"></i></a></li>
-                            <li><a href="#">1</a></li>
-                            <li class="active"><a href="#">2</a></li>
-                            <li><a href="#">3</a></li>
-                            <li><a href="#" aria-label="Next"><i class="feather-chevron-right"></i></a></li>
+                            @if ($posts->hasMorePages())
+                                <li><a href="{{ $posts->nextPageUrl() }}"><i class="icon-55"></i></a></li>
+                            @else
+                                <li><a href="#" aria-disabled="true"><i class="icon-55"></i></a></li>
+                            @endif
                         </ul>
-                    </nav>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 @endsection
--- resources/views/pages/events.blade.php
+++ resources/views/pages/events.blade.php
@@ -1,88 +1,105 @@
-@extends('layouts.app')
+@extends('layouts.template')
 
-@section('title', '#somosAETH | Events')
+@section('title', '#somosAETH | González Center')
 
-@section('meta-description', 'This is a brief description of the blog page.')
+@section('meta-description', 'This is a brief description of the home page.')
 
-@section('meta-keywords', 'blog, posts, news')
+@section('meta-keywords', 'home, welcome, introduction')
+
+
+<!-- Content here -->
 
 @section('content')
 
 
-    <div class="container" style="margin-top:50px;">
-        <div class="row">
-            @foreach($posts as $post)
-                <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                        <a href="{{ route('post.show', $post->slug) }}"><img class="card-img-top" src="{{ $post->image_url }}"
-                                alt="{{ $post->title_en }}"
-                                style="height: 225px; width: 100%; display: block; object-fit: cover; object-position: left top;">
-                        </a>
+    <div class="slider-area rbt-banner-10 height-750 bg_image" data-black-overlay="5"
+        style="background-image: url('{{ asset('assets/images/events/event.jpg') }}')">
 
-                        <div class="card-body d-flex flex-column"
-                            style="min-height: 300px;display: flex;flex-direction: column;justify-content: space-between;">
-                            <div class="d-flex flex-column flex-grow-1">
-                                <p class="card-text" style="margin-bottom:15px;">
-                                    @if(App::getLocale() == 'es')
-                                        {{ $post->title_es }}
-                                    @elseif(App::getLocale() == 'pt-BR')
-                                        {{ $post->title_pt }}
-                                    @else
-                                        {{ $post->title_en }} {{-- Default to English if no locale match --}}
-                                    @endif
-                                </p>
-
-                                <p class="card-text text-muted small flex-grow-1">
-                                    <i>{{ $post->plain_summary }}</i>
-                                </p>
-                            </div>
-
-                            <div class="d-flex justify-content-between align-items-center mt-3">
-                                <div class="btn-group">
-                                    <a href="{{ route('post.show', $post->slug) }}"
-                                        class="btn btn-sm btn-outline-secondary">View</a>
-                                </div>
-                                <small class="text-muted">{{ $post->date }}</small>
-                            </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="inner text-center">
+                        <div class="section-title mb--20">
+                            <span class="subtitle bg-coral-opacity">@lang('messages.calendar')</span>
                         </div>
                     </div>
                 </div>
-            @endforeach
-        </div>
-
-        <div class="pagination-wrapper centred">
-            <ul class="pagination clearfix">
-                @if ($posts->onFirstPage())
-                    <li><a href="#" aria-disabled="true"><i class="icon-56"></i></a></li>
-                @else
-                    <li><a href="{{ $posts->previousPageUrl() }}"><i class="icon-56"></i></a></li>
-                @endif
-
-                @foreach ($posts->getUrlRange(1, $posts->lastPage()) as $page => $url)
-                    <li>
-                        <a href="{{ $url }}" class="{{ $posts->currentPage() === $page ? 'current' : '' }}">
-                            {{ $page }}
-                        </a>
-                    </li>
-                @endforeach
-
-                @if ($posts->hasMorePages())
-                    <li><a href="{{ $posts->nextPageUrl() }}"><i class="icon-55"></i></a></li>
-                @else
-                    <li><a href="#" aria-disabled="true"><i class="icon-55"></i></a></li>
-                @endif
-            </ul>
-        </div>
-    </div>
-
-    <section class="cta-style-two" style="margin-top:75px;">
-        <div class="pattern-layer" style="background-image: url(assets/images/shape/shape-2.png);"></div>
-        <div class="auto-container">
-            <div class="inner-box">
-
-                <h2><i class="bi bi-book" style="font-size: 1.5rem;color:#fff;"></i> @lang('messages.resources_grow'):</h2>
-                <br>
             </div>
         </div>
-    </section>
+    </div>
+    <!-- End Banner Area  -->
+
+    <div class="rbt-counterup-area rbt-section-overlayping-top rbt-section-gapBottom">
+        <div class="container">
+            <div class="row g-5">
+                <!-- Start Single Event  -->
+                @foreach($posts as $post)
+                    <div class="col-lg-6 col-md-6 col-12">
+                        <div class="rbt-card card-list-2 event-list-card variation-01 rbt-hover">
+                            <div class="rbt-card-img">
+                                <a href="{{ route('post.show', $post->slug) }}">
+                                    <img src="{{ $post->image_url }}" alt="{{ $post->title_en }}"
+                                        style="object-fit: cover; height: 250px; width: 100%;">
+                                </a>
+                            </div>
+                            <div class="rbt-card-body">
+                                <ul class="rbt-meta">
+                                    <li><i class="feather-calendar"></i>{{ $post->date }}</li>
+                                    <li><i class="feather-map-pin"></i>{{ $post->location ?? 'Online' }}</li>
+                                </ul>
+                                <h4 class="rbt-card-title">
+                                    <a href="{{ route('post.show', $post->slug) }}">
+                                        @if(App::getLocale() == 'es')
+                                            {{ $post->title_es }}
+                                        @elseif(App::getLocale() == 'pt-BR')
+                                            {{ $post->title_pt }}
+                                        @else
+                                            {{ $post->title_en }}
+                                        @endif
+                                    </a>
+                                </h4>
+                                <p class="text-muted small">{{ $post->plain_summary }}</p>
+                                <div class="read-more-btn mt-2">
+                                    <a class="rbt-btn btn-border hover-icon-reverse btn-sm radius-round"
+                                        href="{{ route('post.show', $post->slug) }}">
+                                        <span class="icon-reverse-wrapper">
+                                            <span class="btn-text">@lang('blog.read_more')</span>
+                                            <span class="btn-icon"><i class="feather-arrow-right"></i></span>
+                                            <span class="btn-icon"><i class="feather-arrow-right"></i></span>
+                                        </span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+                <!-- End Single Event  -->
+
+                <div class="row">
+                    <div class="pagination-wrapper centred">
+                        <ul class="pagination clearfix">
+                            @if ($posts->onFirstPage())
+                                <li><a href="#" aria-disabled="true"><i class="icon-56"></i></a></li>
+                            @else
+                                <li><a href="{{ $posts->previousPageUrl() }}"><i class="icon-56"></i></a></li>
+                            @endif
+
+                            @foreach ($posts->getUrlRange(1, $posts->lastPage()) as $page => $url)
+                                <li>
+                                    <a href="{{ $url }}" class="{{ $posts->currentPage() === $page ? 'current' : '' }}">
+                                        {{ $page }}
+                                    </a>
+                                </li>
+                            @endforeach
+
+                            @if ($posts->hasMorePages())
+                                <li><a href="{{ $posts->nextPageUrl() }}"><i class="icon-55"></i></a></li>
+                            @else
+                                <li><a href="#" aria-disabled="true"><i class="icon-55"></i></a></li>
+                            @endif
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
 @endsection
