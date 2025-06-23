845372a783cb5d44d20649b790ff758115695315
wlsfernandes <wlsfernandes@gmail.com>
2025-06-23 09:09:26 -0400

    fix message onm contatc blade

--- resources/views/pages/contact_us.blade.php
+++ resources/views/pages/contact_us.blade.php
@@ -10,6 +10,7 @@
 <!-- Content here -->
 
 @section('content')
+    <x-alert />
     <section class="contact-info-section bg-color-1 centred">
         <div class="auto-container">
             <div class="row clearfix">
@@ -49,13 +50,13 @@
         <div class="auto-container">
             <!-- iframe div -->
             <!--
-                                                                                                                                                                                                                                                                    <div class="iframe-container" style="margin-bottom: 30px;">
-                                                                                                                                                                                                                                                                        <iframe
-                                                                                                                                                                                                                                                                            src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=PO%20Box%20677848%20Orlando%20FL%2032867&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-                                                                                                                                                                                                                                                                            width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                                                                                                                                                                                                                                                                                                                                                <div class="iframe-container" style="margin-bottom: 30px;">
+                                                                                                                                                                                                                                                                                                                                                    <iframe
+                                                                                                                                                                                                                                                                                                                                                        src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=PO%20Box%20677848%20Orlando%20FL%2032867&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+                                                                                                                                                                                                                                                                                                                                                        width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 
-                                                                                                                                                                                                                                                                    </div>
-                                                                                                                                                                                                                                                            -->
+                                                                                                                                                                                                                                                                                                                                                </div>
+                                                                                                                                                                                                                                                                                                                                        -->
             <div class="row clearfix">
                 <div class="col-lg-4 col-md-12 col-sm-12 content-column">
                     <div class="content-box p_relative mr_70">
@@ -71,7 +72,8 @@
                 </div>
                 <div class="col-lg-8 col-md-12 col-sm-12 form-column">
                     <div class="form-inner">
-                        <form method="post" action="sendemail.php" id="contact-form">
+                        <form method="post" action="{{ route('contact.send') }}" id="contact-form">
+                            @csrf
                             <div class="row clearfix">
                                 <div class="col-lg-6 col-md-6 col-sm-12 form-group">
                                     <input type="text" name="username" placeholder="@lang('messages.your_name')" required>
@@ -80,14 +82,18 @@
                                     <input type="email" name="email" placeholder="@lang('messages.your_email')" required>
                                 </div>
                                 <div class="col-lg-12 col-md-12 col-sm-12 form-group">
-                                    <input type="text" name="phone" required="" placeholder="@lang('messages.your_phone')">
+                                    <input type="text" name="phone" placeholder="@lang('messages.your_phone')" required>
                                 </div>
                                 <div class="col-lg-12 col-md-12 col-sm-12 form-group">
-                                    <textarea name="message" placeholder="@lang('messages.your_message')"></textarea>
+                                    <textarea name="message" placeholder="@lang('messages.your_message')"
+                                        required></textarea>
                                 </div>
+                                {{-- reCAPTCHA --}}
+                                <input type="hidden" name="g-recaptcha-response" id="recaptcha-token">
                                 <div class="col-lg-12 col-md-12 col-sm-12 form-group message-btn mr-0">
-                                    <button class="theme-btn-one" type="submit"
-                                        name="submit-form"><span>@lang('messages.send_message')</span></button>
+                                    <button class="theme-btn-one" type="submit" name="submit-form">
+                                        <span>@lang('messages.send_message')</span>
+                                    </button>
                                 </div>
                             </div>
                         </form>
